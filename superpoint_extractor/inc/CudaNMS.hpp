532940a8154ea6295cf3bb0ddfa0a78ce57b7c20
Khalil L <mortyl0834@gmail.com>
2024-04-25 13:37:18 +0800

    supprt superpoint

--- superpoint_extractor/inc/CudaNMS.hpp
+++
@@ -1,5 +0,0 @@
-#include <cuda_runtime.h>
-#include <iostream>
-#include <chrono>
-void cudaNMS(unsigned char* image, int width, int height, 
-    int neighborhood_size, unsigned char feature_threshold);
